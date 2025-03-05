47248a8c5be275547bb65b29a335e92cba995e96
fxofxo <fxodev@gmail.com>
2025-03-05 20:02:34 +0100

    initial build

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -61,14 +61,15 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(none, default config)" // Original author or contributor.
+#define STRING_CONFIG_H_AUTHOR "(Fxo, SKR Mini E3v3.0)" // Original author or contributor.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 // @section machine
 
 // Choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD BOARD_RAMPS_14_EFB
+  #define MOTHERBOARD BOARD_BTT_SKR_MINI_E3_V3_0
+  //#define MOTHERBOARD BOARD_RAMPS_14_EFB
 #endif
 
 // @section serial
